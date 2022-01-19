04a1d8a13318e1ab3c5004fb6f28479fa79391b8
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-19 11:09:35 +0000

    adds schema.sql

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS department_db;
+
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE department (
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT PRIMARY KEY NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+ALTER TABLE role
+ADD FOREIGN KEY (department_id)
+REFERENCES department (id);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+);
