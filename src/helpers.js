8fc54361b375770db92cd559ad70df44a30f4819
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-21 09:26:09 +0000

    remove return from helper connection functions

--- src/helpers.js
+++ src/helpers.js
@@ -4,7 +4,7 @@ const cTable = require('console.table');
 // View all departments
 const ViewDepartments = () => {
   let str = 'SELECT * FROM department';
-  return connection.query(str, (err, results) => {
+  connection.query(str, (err, results) => {
     if (err) throw err;
     console.table(results); // results contains rows returned by server
   });
@@ -17,7 +17,7 @@ const ViewEmployees = () => {
   JOIN role ON employee.role_id = role.id
   JOIN department ON department.id = role.department_id
   LEFT JOIN employee e2 ON employee.manager_id = e2.id`;
-  return connection.query(str, (err, results) => {
+  connection.query(str, (err, results) => {
     if (err) throw err;
     console.table(results); // results contains rows returned by server
   });
@@ -28,7 +28,7 @@ const ViewRoles = () => {
   let str = `SELECT role.id, role.title, department.name as department, role.salary  
     FROM role 
     JOIN department ON role.department_id = department.id;`;
-  return connection.query(str, (err, results) => {
+  connection.query(str, (err, results) => {
     if (err) throw err;
     console.table(results); // results contains rows returned by server
   });
@@ -36,14 +36,14 @@ const ViewRoles = () => {
 
 const addDepartment = (name) => {
   let str = 'INSERT INTO department (name) VALUES (?)';
-  return connection.query(str, name, (err) => {
+  connection.query(str, name, (err) => {
     if (err) throw err;
   });
 };
 
 const addRole = (title, department_id, salary) => {
   let str = 'INSERT INTO role (title, department_id, salary) VALUES (?, ?, ?)';
-  return connection.query(str, [title, department_id, salary], (err) => {
+  connection.query(str, [title, department_id, salary], (err) => {
     if (err) throw err;
   });
 };
@@ -51,19 +51,15 @@ const addRole = (title, department_id, salary) => {
 const addEmployee = (first_name, last_name, role_id, manager_id) => {
   let str =
     'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
-  return connection.query(
-    str,
-    [first_name, last_name, role_id, manager_id],
-    (err) => {
-      if (err) throw err;
-    }
-  );
+  connection.query(str, [first_name, last_name, role_id, manager_id], (err) => {
+    if (err) throw err;
+  });
 };
 
 const updateEmployeeRole = (role_id, first_name, last_name) => {
   let str =
     'UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?';
-  return connection.query(str, [role_id, first_name, last_name], (err) => {
+  connection.query(str, [role_id, first_name, last_name], (err) => {
     if (err) throw err;
   });
 };
